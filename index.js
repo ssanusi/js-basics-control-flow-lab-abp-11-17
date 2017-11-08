1293e3c18b51d6a8417d9bd0975de39e398938dd
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-08 16:53:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ function ternaryCheckCity(city){
   return city === "NYC" ? "Ok, sounds good." : "No go." ;
 }
 
-// function switchOnCharmFromTip(tips){
-//   switch (tips) {
-//     case tips === "generous":
-//       return "Thanks you so much.";
-//       break;
-//     case tips !=== "generous" :
-//         return "Thank you.";
-//         break;
-//     default:
-//       return "bye."  
-//   }
-// }
+function switchOnCharmFromTip(tips){
+  switch (tips) {
+    case "generous":
+      return "Thanks you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "bye."
+  }
+}
