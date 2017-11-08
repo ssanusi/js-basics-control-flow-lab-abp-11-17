ba1b978f0e685f968dae2dc0e878250f1d5cc461
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-08 16:49:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+function scuberGreetingForFeet(num){
+  if(num > 2500){
+    return 'No can do.';
+  }
+  else if(num > 400){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'This one is on me!';
+  }
+}
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go." ;
+}
+
+// function switchOnCharmFromTip(tips){
+//   switch (tips) {
+//     case tips === "generous":
+//       return "Thanks you so much.";
+//       break;
+//     case tips !=== "generous" :
+//         return "Thank you.";
+//         break;
+//     default:
+//       return "bye."  
+//   }
+// }
