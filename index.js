b47ae67ee69739894a413ecbad49f35bb79cfbcd
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-08 16:54:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+function scuberGreetingForFeet(num){
+  if(num > 2500){
+    return 'No can do.';
+  }
+  else if(num > 400){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'This one is on me!';
+  }
+}
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go." ;
+}
+
+function switchOnCharmFromTip(tips){
+  switch (tips) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+  }
+}
